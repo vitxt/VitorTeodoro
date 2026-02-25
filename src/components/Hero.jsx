808432a0c5a7877f9ae4dc3afcea1953b8a03d23
vitxt <teodorovitor30@gmail.com>
2026-02-24 21:26:16 -0300

    mobile fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ export default function Hero() {
     {
       title: "Hello",
       description:
-      "I'm Vitor Teodoro, a Computer Science student at State University of Maringá (UEM).",
+      "I'm Vitor Teodoro, a brazilian Computer Science student at State University of Maringá (UEM).",
     },
     {
       title: "Goals",
@@ -86,40 +86,40 @@ export default function Hero() {
   };
 
   return (
-    <div className="relative h-screen bg-[#0a0a0c] text-white overflow-hidden flex items-center px-10 md:px-20">
+    <div className="relative h-screen bg-[#0a0a0c] text-white overflow-hidden flex items-center px-5 md:px-20">
       {/* 1. Background Image Layer */}
-      <div className="absolute top-0 right-0 w-[55%] h-full z-0 pointer-events-none">
+      <div className="absolute top-0 right-0 w-[55%] md:w-[55%] h-full z-0 pointer-events-none">
         <div className="absolute inset-0 bg-gradient-to-r from-[#0a0a0c] via-transparent to-transparent z-10" />
         <div className="absolute inset-0 bg-gradient-to-t from-[#0a0a0c] via-transparent to-transparent z-10" />
         <img
           src={img}
           alt="Vitor Teodoro"
-          className="object-cover w-full h-full grayscale opacity-80"
+          className="object-cover w-full h-full  grayscale opacity-80"
         />
       </div>
 
       {/* 2. Left Content Container */}
-      <div className="relative z-10 max-w-xl flex flex-col justify-center min-h-[400px]">
+      <div className="relative z-10 max-w-xl flex flex-col mb-100 md:mb-0 justify-end md:justify-center h-full ">
         
         <div className={`transition-opacity duration-500 ease-in-out ${isVisible ? "opacity-100" : "opacity-0"}`}>
           
-          <h1 className="text-7xl font-semibold mb-6 tracking-wide">
+          <h1 className="text-4xl md:text-7xl font-semibold mb-2 md:mb-6 tracking-wide">
             {topics[activeIndex].title}
           </h1>
           
-          <div className="w-12 h-1 bg-white mb-8 rounded-full"></div>
+          <div className="w-8 md:w-12 h-0.5 md:h-1 bg-white mb-3 md:mb-8 rounded-full"></div>
 
           {topics[activeIndex].description && (
-            <p className="text-gray-400 text-lg leading-relaxed font-light pr-8 min-h-[80px]">
+            <p className="text-gray-400 text-md md:text-lg leading-8 md:leading-relaxed font-light pr-8 min-h-[150px] md:min-h-[80px]">
               {topics[activeIndex].description}
             </p>
           )}
 
           {topics[activeIndex].stack && (
-            <div className="grid grid-cols-4 gap-6 mt-8 animate-fade-in-up">
+            <div className=" grid grid-cols-4 gap-6 animate-fade-in-up ">
               {topics[activeIndex].stack.map((tech, index) => (
                 <div key={index} className="group relative flex flex-col items-center">
-                  <div className="p-3 bg-gray-800/50 rounded-xl border border-gray-700 hover:border-white/50 transition-all duration-300 hover:scale-110 hover:bg-gray-700/80 cursor-pointer">
+                  <div className="p-2 bg-gray-800/50 rounded-xl border border-gray-700 hover:border-white/50 transition-all duration-300 hover:scale-110 hover:bg-gray-700/80 cursor-pointer">
                     <tech.Icon
                       className="w-8 h-8 opacity-80 group-hover:opacity-100 transition-opacity"
                       style={{ color: tech.color }}
@@ -138,7 +138,7 @@ export default function Hero() {
       </div>
 
       {/* 3. Right Navigation Bars */}
-      <div className="absolute right-10 top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-20 items-end">
+      <div className="absolute right-10 top- md:top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-20 items-end">
         {topics.map((_, index) => (
           <div
             key={index}
@@ -146,7 +146,7 @@ export default function Hero() {
             onClick={() => handleManualChange(index)} 
           >
             {index === activeIndex ? (
-              <div className="h-[3px] w-10 bg-gray-800 rounded overflow-hidden">
+              <div className="h-[3px] w-5 md:w-10 bg-gray-800 rounded overflow-hidden">
                 <div
                   className="h-full bg-white"
                   style={{
@@ -156,7 +156,7 @@ export default function Hero() {
                 ></div>
               </div>
             ) : (
-              <div className="h-0.75 w-8 bg-gray-700 rounded transition-all duration-300 group-hover:bg-gray-400 group-hover:w-8"></div>
+              <div className="h-0.75 w-4 md:w-8 bg-gray-700 rounded transition-all duration-300 group-hover:bg-gray-400 group-hover:w-6 md:group-hover:w-8"></div>
             )}
           </div>
         ))}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 
-
 export default function NavBar() {
-  const myEmail = "vitorteodorodev@gmail.com"; 
+  const myEmail = "vitorteodorodev@gmail.com";
 
-  const [copiedState, setCopiedState] = useState(null); 
+  const [copiedState, setCopiedState] = useState(null);
 
   const handleCopy = (text, type) => {
     navigator.clipboard.writeText(text);
     setCopiedState(type);
-    
 
     setTimeout(() => {
       setCopiedState(null);
@@ -23,45 +21,43 @@ export default function NavBar() {
       <div className="text-md font-semibold tracking-[0.2em] text-gray-500">
         Vitor Teodoro
       </div>
-      
+
       <div className="flex items-center space-x-8">
-        
-        <a 
-          href="https://www.linkedin.com/in/vitorteodorodev" 
-          target="_blank" 
+        <a
+          href="https://www.linkedin.com/in/vitorteodorodev"
+          target="_blank"
           rel="noopener noreferrer"
-          className=" text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
+          className=" text-amber-50 md:text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
         >
-          <FaLinkedinIn className="w-6 h-6"  />
+          <FaLinkedinIn className="w-6 h-6" />
         </a>
 
-        <a 
-          href="https://github.com/vitxt" 
-          target="_blank" 
+        <a
+          href="https://github.com/vitxt"
+          target="_blank"
           rel="noopener noreferrer"
-          className=" text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
+          className="text-amber-50 md:text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
         >
-          <FaGithub className="w-6 h-6"  />
+          <FaGithub className="w-6 h-6" />
         </a>
 
         <div className="relative flex flex-col items-center">
-          <button 
-            onClick={() => handleCopy(myEmail, 'email')}
-            className="text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
+          <button
+            onClick={() => handleCopy(myEmail, "email")}
+            className="text-amber-50 md:text-black/45 hover:text-white transition-colors hover:scale-110 focus:outline-none"
             title="Copy Email"
           >
             <MdAlternateEmail className="w-6 h-6 fill-current" />
           </button>
-          
-          {copiedState === 'email' && (
-            <span className="absolute -bottom-8 px-2 py-1 text-xs text-white bg-gray-800 rounded shadow-lg animate-fade-in-up whitespace-nowrap">
-              Email Copied!
+
+          {copiedState === "email" && (
+            <span className="absolute -bottom-8 px-2 py-1 text-[0.7rem] md:text-xs text-white bg-gray-800 rounded shadow-lg animate-fade-in-up whitespace-nowrap">
+              <span className="md:hidden">Copied!</span>
+              <span className="hidden md:inline">Email Copied!</span>
             </span>
           )}
         </div>
-
       </div>
     </nav>
   );
 }
-
